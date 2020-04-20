f4b67ba48b36d9af00895899db2aa87930a95869
Gabriel Lima F. de Souza <gabriellfsouza@gmail.com>
2020-04-20 04:13:16 -0300

    challenge finished

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,11 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if (total - value < 0 && type === 'outcome')
+      throw new Error(`You can't spend more than your balance.`);
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
